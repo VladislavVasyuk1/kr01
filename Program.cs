b7518aeb610cb1fdeac5043330561daf2b9e8ae1
VladislavVasyuk1 <139385380+VladislavVasyuk1@users.noreply.github.com>
2023-11-07 17:32:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array1 = new string[] {"вв", "уццу", "вмы", "44", "ывм", "вы", "3535"};
+string[] array2 = new string[array1.Length];
+
+void sorted(string[] array1, string[] array2)
+{
+    int x = 0;
+    for(int i = 1; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+                {
+                    array2[x] = array1[i];
+                    x++;
+                }
+        }
+}
+
+void WriteArray(string[] array)  // вывод массива
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+sorted(array1, array2);
+WriteArray(array2);
+
+
+
+
+
+
+
